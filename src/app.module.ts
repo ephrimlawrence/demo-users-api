b147c293d92c042db0990458a73e23db658b8db4
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-02-01 12:29:46 +0000

    feat: setup request rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { UserService } from './users.service';
 import { KindagooseModule } from 'kindagoose';
 import { User } from './models/user.model';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    // Configure the ThrottlerModule for the entire application, with a limit of 10 requests per minute
+    ThrottlerModule.forRoot([{
+      ttl: 60 * 1000, // 1 minute
+      limit: 10,
+    }]),
 
     //! NOTE: Recommended to use environment variables here. This is just for demonstration purposes.
     KindagooseModule.forRoot('mongodb://localhost:27017', {
@@ -21,6 +27,12 @@ import { APP_FILTER } from '@nestjs/core';
   controllers: [UserController],
   providers: [
     UserService,
+
+
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
 
     {
       provide: APP_FILTER,
